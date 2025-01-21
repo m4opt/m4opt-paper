2d2437d803576e7149b48ae8eb2d02acc5419c46
Leo Singer <leo.singer@ligo.org>
2025-01-21 15:50:22 -0500

    Add sample table

---
+++ scripts/plan-table.py
@@ -0,0 +1,42 @@
+from astropy.table import QTable
+import numpy as np
+from astropy import units as u
+
+table = QTable.read("runs_SNR-10/O5HLVK/farah/allsky/3628.ecsv")
+table["start_time"].precision = 0
+with open("tables/3628.tex", "w") as f:
+    for i, row in enumerate(table):
+        print(
+            row["start_time"],
+            int(np.rint(row["duration"].to_value(u.s))),
+            row["action"],
+            "---"
+            if row["observer_location"].mask
+            else int(np.rint(
+                row["observer_location"].geocentric[0].to_value(u.km))),
+            "---"
+            if row["observer_location"].mask
+            else int(np.rint(
+                row["observer_location"].geocentric[1].to_value(u.km))),
+            "---"
+            if row["observer_location"].mask
+            else int(np.rint(
+                row["observer_location"].geocentric[2].to_value(u.km))),
+            "---"
+            if row["target_coord"].mask
+            else np.format_float_positional(
+                row["target_coord"].ra.deg, fractional=True, trim="k", precision=4, min_digits=4,
+            ),
+            "---"
+            if row["target_coord"].mask
+            else np.format_float_positional(
+                row["target_coord"].dec.deg, fractional=True, trim='k', precision=4, min_digits=4, sign=True
+            ),
+            "---"
+            if row["roll"].mask
+            else int(np.rint(
+                row["roll"].to_value(u.deg))),
+            sep=" & ",
+            end=' \\\\\n' if i < len(table) - 1 else '\n',
+            file=f,
+        )
