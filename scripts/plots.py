3bf2f794c0adf842a142f06e27c55febf57582e2
Leo Singer <leo.singer@ligo.org>
2025-01-25 11:14:02 -0500

    Switch to serif fonts in plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,8 +1,13 @@
 """Matplotlib style customization."""
+
 from matplotlib import pyplot as plt
+
 
 def customize_style():
     target_width = 3.5  # ApJ column size in inches
     width, height = plt.rcParams["figure.figsize"]
     plt.style.use("seaborn-v0_8-paper")
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.serif"] = "Times New Roman"
+    plt.rcParams['mathtext.fontset'] = 'stix'
     plt.rcParams["figure.figsize"] = (target_width, height * target_width / width)
