e177075e4f97d418b860d8879935abb39eb7d247
Leo Singer <leo.singer@ligo.org>
2025-02-05 01:13:38 -0500

    Add support for 2-column plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -3,8 +3,11 @@
 from matplotlib import pyplot as plt
 
 
-def customize_style():
-    target_width = 3.5  # ApJ column size in inches
+def customize_style(columns=1):
+    if columns == 1:
+        target_width = 3.5  # ApJ column size in inches
+    else:
+        target_width = 7.25  # ApJ two-column text width in inches
     width, height = plt.rcParams["figure.figsize"]
     plt.style.use("seaborn-v0_8-paper")
     plt.rcParams["font.family"] = "serif"
